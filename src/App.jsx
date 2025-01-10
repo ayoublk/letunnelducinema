e512d37b9c63d8eb0a928db1455ea345b5df914a
ayoublk <ayoub41200@hotmail.fr>
2025-01-11 00:54:13 +0100

    remise score victoire à 25

--- src/App.jsx
+++ src/App.jsx
@@ -729,7 +729,7 @@ function App() {
     newPlayers[currentPlayer].score += currentPoints;
     setPlayers(newPlayers);
     
-    if (newPlayers[currentPlayer].score >= 2) {
+    if (newPlayers[currentPlayer].score >= 25) {
       playSound('victory');
       setShowConfetti(true);
       setWinners([newPlayers[currentPlayer]]); // Ajout de cette ligne
